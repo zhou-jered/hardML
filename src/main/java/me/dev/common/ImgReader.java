0b834dcb996b7f051cca234662df62235d846502
zhou-jered <cyzjylw@126.com>
2021-11-12 21:33:49 +0800

    dev

--- src/main/java/me/dev/common/ImgReader.java
+++ src/main/java/me/dev/common/ImgReader.java
@@ -1,4 +1,28 @@
 package me.dev.common;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ImgReader {
+
+    public static MyImage read(File file) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(file);
+        System.out.println(bufferedImage.getData().getNumBands());
+        System.out.println(bufferedImage.getColorModel());
+        System.out.println(bufferedImage.getData().getBounds());
+        return null;
+    }
+
+    public static void main(String[] args) {
+        File file = new File("images/ChengHua.png");
+        System.out.println(file.exists());
+        try {
+            read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
